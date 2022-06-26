b822941ecf0d3af8f3e37003adb10f6610a72294
Woga65 <woga@mytsa.de>
2022-06-26 12:33:10 +0200

    make mapping capabilities configureable in case we dont need it

--- src/main/rx/rx.c
+++ src/main/rx/rx.c
@@ -71,7 +71,12 @@
 
 //#define DEBUG_RX_SIGNAL_LOSS
 
-const char rcChannelLetters[] = "AERT1234"; // "AERT" sibi?
+#if (MAX_MAPPABLE_RX_INPUTS == 4) // sibi?
+const char rcChannelLetters[] = "AERT";
+#else
+const char rcChannelLetters[] = "AERT1234";
+#endif
+
 
 static uint16_t rssi = 0;                  // range: [0;1023]
 static timeUs_t lastMspRssiUpdateUs = 0;
@@ -124,7 +129,11 @@ PG_REGISTER_WITH_RESET_TEMPLATE(rxConfig_t, rxConfig, PG_RX_CONFIG, 10);
 #define RX_MIN_USEX 885
 PG_RESET_TEMPLATE(rxConfig_t, rxConfig,
     .receiverType = DEFAULT_RX_TYPE,
-    .rcmap = {0, 1, 3, 2, 4, 5, 6, 7},  // {0, 1, 3, 2) sibi?    // Default to AETR1234 map
+#if (MAX_MAPPABLE_RX_INPUTS == 4) // sibi?
+    .rcmap = {0, 1, 3, 2},  // Default to AETR map
+#else
+    .rcmap = {0, 1, 3, 2, 4, 5, 6, 7},  // Default to AETR1234 map
+#endif
     .halfDuplex = SETTING_SERIALRX_HALFDUPLEX_DEFAULT,
     .serialrx_provider = SERIALRX_PROVIDER,
 #ifdef USE_RX_SPI
